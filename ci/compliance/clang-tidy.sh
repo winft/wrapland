c2e53da61a1f473bb1c8985841c4d2b2fae0cba8
Roman Gilg <subdiff@gmail.com>
2020-04-03 15:38:35 +0200

    ci: add clang linters
    Linters based on clang-format and clang-tidy are introduced.
    
    For clang-format the current KF5 .clang-format file is reused and slightly
    adapted.
    
    The clang-tidy linter is run after build because it needs the compilation
    database, something that CMake will provide for it.
    
    See also issue #17.

---
+++ ci/compliance/clang-tidy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eux
+set -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SOURCE_DIR=$(dirname $(dirname ${SCRIPT_DIR}))
+
+python2 $SCRIPT_DIR/run-clang-tidy.py \
+    -header-filter="${SOURCE_DIR}/server/" -warnings-as-errors="*" \
+    -checks="*, \
+        -fuchsia-default-arguments-calls, \
+        -fuchsia-default-arguments-declarations, \
+        -google-readability-namespace-comments, \
+        -llvm-namespace-comment, \
+        -llvm-header-guard, \
+        -modernize-use-trailing-return-type \
+        " \
+    ${SOURCE_DIR}/server \
+    ${SOURCE_DIR}/autotests/client/output.cpp \
+    ${SOURCE_DIR}/autotests/server/display.cpp
