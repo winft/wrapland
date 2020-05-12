2b623088071fa3b32405268381ea0566402dcb0d
Roman Gilg <subdiff@gmail.com>
2020-05-12 22:43:48 +0200

    ci: update clang-tidy configuration
    This moves the clang-tidy configuration into a separate file in the source
    root directory. The script must be called from this directory and the path to
    the build directory provided, otherwise it is assumed the path is `../build`.
    
    The clang-tidy checks we want to run are selected in the configuration file.

--- ci/compliance/clang-tidy.sh
+++ ci/compliance/clang-tidy.sh
@@ -6,29 +6,13 @@ set -o pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SOURCE_DIR=$(dirname $(dirname ${SCRIPT_DIR}))
 
+if [ $# -eq 0 ] ; then
+  BUILD_DIR='../build'
+else
+  BUILD_DIR="$1"
+fi
+
 python2 $SCRIPT_DIR/run-clang-tidy.py \
-    -header-filter="${SOURCE_DIR}/server/" -warnings-as-errors="*" \
-    -checks="*, \
-        -fuchsia-default-arguments-calls, \
-        -fuchsia-default-arguments-declarations, \
-        -google-readability-namespace-comments, \
-        -llvm-namespace-comment, \
-        -llvm-header-guard, \
-        -modernize-use-trailing-return-type \
-        " \
-    ${SOURCE_DIR}/server \
-    ${SOURCE_DIR}/autotests/client/compositor.cpp \
-    ${SOURCE_DIR}/autotests/client/data_device.cpp \
-    ${SOURCE_DIR}/autotests/client/data_source.cpp \
-    ${SOURCE_DIR}/autotests/client/drag_and_drop.cpp \
-    ${SOURCE_DIR}/autotests/client/output.cpp \
-    ${SOURCE_DIR}/autotests/client/region.cpp \
-    ${SOURCE_DIR}/autotests/client/seat.cpp \
-    ${SOURCE_DIR}/autotests/client/selection.cpp \
-    ${SOURCE_DIR}/autotests/client/subcompositor.cpp \
-    ${SOURCE_DIR}/autotests/client/subsurface.cpp \
-    ${SOURCE_DIR}/autotests/client/surface.cpp \
-    ${SOURCE_DIR}/autotests/client/xdg_decoration.cpp \
-    ${SOURCE_DIR}/autotests/client/xdg_foreign.cpp \
-    ${SOURCE_DIR}/autotests/client/xdg_shell.cpp \
-    ${SOURCE_DIR}/autotests/server/display.cpp
+    -p="$BUILD_DIR" \
+    -header-filter="${SOURCE_DIR}/server/" \
+    ${SOURCE_DIR}/server
