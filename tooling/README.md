1cb1fdc5894bc1904fa8900538e3501b06f44db5
Roman Gilg <subdiff@gmail.com>
2021-03-03 17:55:26 +0100

    ci: include job templates for static analysis
    The static analysis infrastructure has been centralized in the tooling repo.
    Use the job template from there and rearrange our internal files for it.

---
+++ tooling/README.md
@@ -0,0 +1,8 @@
+# Wrapland Tooling
+## Analysis
+Uses the analysis functionality from the [tooling repo][kwinft-tooling].
+
+The provided scripts can be run locally, too.
+Additional tools are downloaded temporarily from the tooling repo.
+
+[kwinft-tooling]: https://gitlab.com/kwinft/tooling
--- tooling/analysis/clang-tidy.sh
+++ tooling/analysis/clang-tidy.sh
@@ -5,14 +5,16 @@ set -o pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SOURCE_DIR=$(dirname $(dirname ${SCRIPT_DIR}))
+RUN_SCRIPT_URL="https://gitlab.com/kwinft/tooling/-/raw/master/analysis/run-clang-tidy.py"
 
 if [ $# -eq 0 ] ; then
-  BUILD_DIR='../build'
+  BUILD_DIR='build'
 else
   BUILD_DIR="$1"
 fi
 
-python2 $SCRIPT_DIR/run-clang-tidy.py \
+cd $SOURCE_DIR
+python2 <(curl -s $RUN_SCRIPT_URL) \
     -p="$BUILD_DIR" \
     -header-filter="${SOURCE_DIR}/server/" \
     ${SOURCE_DIR}/server
