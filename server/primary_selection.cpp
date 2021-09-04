c4a4158b8f31deb21184301bf483aa574fccdb77
Roman Gilg <subdiff@gmail.com>
2021-09-04 03:27:34 +0200

    refactor(server): consolidate primary selection source files
    The primary selection device manager source file seems unnecessary as the main
    primary selection source file is already not very big.
    
    Move the manager definition to the main source file and delete the other file.

--- server/primary_selection.cpp
+++ server/primary_selection.cpp
@@ -7,6 +7,7 @@
 #include "primary_selection_p.h"
 
 #include "client.h"
+#include "display.h"
 #include "seat_p.h"
 #include "selection_p.h"
 
@@ -14,6 +15,58 @@
 
 namespace Wrapland::Server
 {
+
+const struct zwp_primary_selection_device_manager_v1_interface
+    PrimarySelectionDeviceManager::Private::s_interface
+    = {
+        cb<create_source>,
+        cb<get_device>,
+        resourceDestroyCallback,
+};
+
+PrimarySelectionDeviceManager::Private::Private(Display* display, PrimarySelectionDeviceManager* q)
+    : device_manager<PrimarySelectionDeviceManagerGlobal>(
+        q,
+        display,
+        &zwp_primary_selection_device_manager_v1_interface,
+        &s_interface)
+{
+    create();
+}
+
+PrimarySelectionDeviceManager::Private::~Private() = default;
+
+PrimarySelectionDeviceManager::PrimarySelectionDeviceManager(Display* display, QObject* parent)
+    : QObject(parent)
+    , d_ptr(new Private(display, this))
+{
+}
+
+PrimarySelectionDeviceManager::~PrimarySelectionDeviceManager() = default;
+
+void PrimarySelectionDeviceManager::create_source(Client* client, uint32_t version, uint32_t id)
+{
+    auto source = new source_t(client, version, id);
+    if (!source) {
+        return;
+    }
+
+    Q_EMIT sourceCreated(source);
+}
+
+void PrimarySelectionDeviceManager::get_device(Client* client,
+                                               uint32_t version,
+                                               uint32_t id,
+                                               Seat* seat)
+{
+    auto device = new PrimarySelectionDevice(client, version, id, seat);
+    if (!device) {
+        return;
+    }
+
+    seat->d_ptr->primary_selection_devices.register_device(device);
+    Q_EMIT deviceCreated(device);
+}
 
 const struct zwp_primary_selection_device_v1_interface PrimarySelectionDevice::Private::s_interface
     = {
