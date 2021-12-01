c873a34382cf7d4ac8dcf9119d97c775fa23b32c
Roman Gilg <subdiff@gmail.com>
2021-12-01 21:59:36 +0100

    ci: load run-clang-tidy script from official source
    Instead of using the outdated copy in the tooling repository we can directly
    load the version from the official LLVM repo.
    
    As since this newer version of the script is already compatible with Python 3,
    we can use this immediately instead of running it through Python 2.

--- tooling/analysis/clang-tidy.sh
+++ tooling/analysis/clang-tidy.sh
@@ -5,7 +5,8 @@ set -o pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SOURCE_DIR=$(dirname $(dirname ${SCRIPT_DIR}))
-RUN_SCRIPT_URL="https://gitlab.com/kwinft/tooling/-/raw/master/analysis/run-clang-tidy.py"
+RUN_SCRIPT_URL="https://raw.githubusercontent.com/llvm/llvm-project/release/13.x/\
+clang-tools-extra/clang-tidy/tool/run-clang-tidy.py"
 
 if [ $# -eq 0 ] ; then
   BUILD_DIR='build'
@@ -14,7 +15,7 @@ else
 fi
 
 cd $SOURCE_DIR
-python2 <(curl -s $RUN_SCRIPT_URL) \
+python <(curl -s $RUN_SCRIPT_URL) \
     -p="$BUILD_DIR" \
     -header-filter="^${SOURCE_DIR}/server/" \
     ${SOURCE_DIR}/server
